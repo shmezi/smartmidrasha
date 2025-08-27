e12eea11d7b35c87cb0f2086dce9a285aa692fa6
Ezra Golombek <ezragolombek.main@gmail.com>
2025-08-27 15:19:21 +0300

    Added new page for overviewing the status of the current day's house review.

--- src/app/actions/logActions.ts
+++ src/app/actions/logActions.ts
@@ -3,13 +3,12 @@ import {AttendanceLog, IAttendanceLog} from "@/struct/impl/AttendanceLog";
 
 export const createUserLog = async (formData: FormData) => {
     const commanderId = formData.get('commanderId') as string
-    const absent = formData.getAll('absent') as string[]
     const present = formData.getAll('present') as string[]
 
     const log: IAttendanceLog = {
         commanderId: commanderId,
         date: new Date().toLocaleDateString(),
-        absent: absent,
+
         present: present
     }
     await AttendanceLog.insertOne(log)
---
+++ src/app/commander/homes/dash/widget/Widget.tsx
@@ -0,0 +1,22 @@
+import {Card, Grid} from "@mui/material";
+import React from "react";
+
+const Widget = (
+    {
+        children,
+        width,
+        aspectRatio,
+
+    }: {
+        children: React.ReactNode | undefined
+        width: number,
+        aspectRatio: number
+    }) => {
+    return <Grid size={width}>
+        <Card sx={{aspectRatio:  (aspectRatio)}}>
+            {children}
+        </Card>
+    </Grid>
+
+}
+export default Widget
